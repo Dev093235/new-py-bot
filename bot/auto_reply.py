35d31246cd6bdd9ba092b7c2d29a739f886ccd14
Dev093235 <dev431140@gmail.com>
2025-03-06 16:54:23 +0530

    Update auto_reply.py

--- bot/auto_reply.py
+++ bot/auto_reply.py
@@ -1,1 +1,17 @@
+import random
+import time
+from bot.utils import get_random_reply
 
+def auto_reply(message, sender_name):
+    """
+    Automatically generates a flirty or funny reply based on the received message.
+    Uses a predefined list of responses from utils.py.
+    """
+    reply = get_random_reply(message)
+    if sender_name:
+        reply = f"{sender_name}, {reply}"
+    
+    # Simulating typing delay
+    time.sleep(random.uniform(1, 3))
+    
+    return reply
