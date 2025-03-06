c8339de5b97ed5aa3332008c6786257170ff72b9
Dev093235 <dev431140@gmail.com>
2025-03-06 17:28:53 +0530

    Update restart.py

--- restart.py
+++ restart.py
@@ -1,1 +1,11 @@
+import os
+import sys
+import time
 
+def restart_program():
+    print("🔄 Restarting bot...")
+    time.sleep(2)
+    os.execv(sys.executable, ['python'] + sys.argv)
+
+if __name__ == "__main__":
+    restart_program()
