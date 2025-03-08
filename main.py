e859d98a4815848d896ade92ccd2766b44de7225
Dev093235 <dev431140@gmail.com>
2025-03-08 17:02:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from bot.fb_login import get_facebook_session
 import time
 
 print("🔥 Mohit Bot Starting...")
-
 session = get_facebook_session()
+
 if not session:
     print("❌ Exiting... Login Failed.")
-    exit(1)  # 🛑 **Process exit karne ke liye**
 else:
-    print("✅ Bot is now active!")
-    while True:
-        print("💬 Bot running... Checking messages!")
-        time.sleep(10)  # **10 sec delay to avoid spam**
+    print("✅ Successfully Logged in!")
